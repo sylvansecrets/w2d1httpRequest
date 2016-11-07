616af1350dc7f7e307bf03cfb53a791507b81811
Richard Zhang <lapislacrima@gmail.com>
2016-11-07 18:12:35 +0000

    prints the inputs without line breaks

--- printChunk.js
+++ printChunk.js
@@ -6,7 +6,7 @@ function getAndPrintHTMLChunks () {
 
   var requestOptions = {
     host: 'sytantris.github.io',
-    path: '/http-examples/step1.html'
+    path: '/http-examples/step2.html'
   };
 
   /* Add your code here */
@@ -14,17 +14,19 @@ function getAndPrintHTMLChunks () {
 
 
   https.get(requestOptions, function (response){
+    // sets a mutable list as the output
+    outputList = [];
 
     // sets the encoding of the received data to utf-8
     response.setEncoding('utf8');
 
     // console.log the data when it is received
     response.on('data', function (data){
-      console.log(data.length + "\n");
+      outputList.push(data.toString());
     });
 
     response.on('end', function(){
-      console.log('Response stream complete.')
+      console.log(outputList.join(""));
     });
 
     response.on('err', function(err){
